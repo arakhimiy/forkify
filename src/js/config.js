1f23c07508f6f3b9cec5c6919e1cf00c04476bc1
arakhimiy <azizrahimov9999@gmail.com>
2021-07-25 20:56:54 +0500

    forkify app| build with vanilla JavaScript

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '318acacb-9a3c-49ca-a8e4-fbbb600e199f';
+export const MODAL_CLOSE_SEC = 2.5;
